03120b1d23e627e5e8684edc19425969e1821410
RenatoVR1998 <renato_vianareis@hotmail.com>
2022-09-08 13:22:12 -0300

    subir o exercício Plano de Saúde

---
+++ index-exercício2.js
@@ -0,0 +1,17 @@
+let PS = "Plano de Saúde"; N = "Nome: "; A = "Altura: "; P = "Peso: "; DI = "Data de Internação: "; D = "Diabético? "
+alert(PS)
+
+console.log(N)
+N + prompt("Nome: ")
+
+console.log(A)
+A + prompt("Altura: ")
+
+console.log(P)
+P + prompt("Peso: ")
+
+console.log(DI)
+DI + prompt("Data de Internação: ")
+
+console.log(D)
+D + prompt("Diabético? ")
